f9ffb7b0ec157b356d63ffe421dc1fb32c2f52fb
Diego-Esquivias <desqui048@west-mec.org>
2024-09-26 13:57:36 -0700

    Update 04-params-query.js

--- Express/04-params-query.js
+++ Express/04-params-query.js
@@ -19,14 +19,42 @@ app.get('/api/products', (req, res) => {
 // This is how you set up your params for the data query 
 
 app.get('/api/products/:productID', (req, res) => {
-    console.log(req.params)
+    console.log(req.query)
     const {productID} = req.params
     const singleProduct = products.find((product) => product.id === Number(productID))
     
     if(!singleProduct){
         return res.status(404).send('Product not found/item does not exist')
+    } else if (req.query.Life == '42') {
+        return res.status(202).send("You have found the easter egg")
     }
     return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    console.log(Number(req.params.productID))
+    res.send("This product has been reviewed by a person: its the best there is 10/10 would buy again")
+})
+
+// Sets up a query that you can grab
+app.get('/api/vi/query', (req, res) => {
+    // console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1) {
+        return res.status(200).json({success:true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, () => {
